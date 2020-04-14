c1d4f52fe173478bef1f5b71b6abba4ff1f4dbb2
sp <victor@sp.com>
2020-04-14 18:55:34 +0530

    Commit chec, version 2

--- pd_api/urls.py
+++ pd_api/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-from rest_framework.authtoken.views import obtain_auth_token
 from .api import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
