f2af56146a25646a3e37983387107befa95cc891
sp <victor@sp.com>
2020-04-14 19:56:31 +0530

    views updated

--- incident/views.py
+++ incident/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from . import models
 from . import serializers
 
 
 class Incidents(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,) 
-    
     queryset = models.Incidents.objects.all()
     serializer_class = serializers.IncidentsSerializer
