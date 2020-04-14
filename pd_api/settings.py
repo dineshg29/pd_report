3e93d95ccc95022e85fb605a559c75d4a7759bce
sp <victor@sp.com>
2020-04-14 18:43:14 +0530

    update

--- pd_api/settings.py
+++ pd_api/settings.py
@@ -39,15 +39,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'incident',
     'rest_framework',
+    'djoser',
     'rest_framework.authtoken',
 ]
 
 
 REST_FRAMEWORK = {
 
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated',
+    ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 
