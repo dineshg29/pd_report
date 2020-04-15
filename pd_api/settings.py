3e02bc6a6e71d850635f498214ec7d72d4ca4411
sp <victor@sp.com>
2020-04-15 13:50:13 +0530

    git ignore updated

--- pd_api/settings.py
+++ pd_api/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'pd_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/rest_framework')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
